d7d282fe8f797e4dc0a6dcefb6d73eed26c519f3
Indu-H <167083243+Indu-H@users.noreply.github.com>
2024-04-27 16:51:02 +0530

    Delete head node in DoublyLinkedList.py

---
+++ Delete head node in DoublyLinkedList.py
@@ -0,0 +1,77 @@
+class Box:
+   def __init__(self, data):
+       self.data = data
+       self.prev = None
+       self.next = None
+
+
+      
+def findTail(head):
+   if head == None:
+       return None
+   tail = head
+   while tail.next != None:
+       tail = tail.next
+   return tail
+
+
+def insertAtEnd(head, ele):
+   newNode = Box(ele)
+   if head == None:
+       return newNode
+   tail = findTail(head)
+   tail.next = newNode
+   newNode.prev = tail
+   return head
+
+
+def printLeftToRight(head):
+   curr = head
+   while curr != None:
+       print(curr.data, end = " ")
+       curr = curr.next
+   print()
+  
+def printRightToLeft(head):
+   tail = findTail(head)
+   while tail != None:
+       print(tail.data, end = " ")
+       tail = tail.prev
+   print()
+      
+def deleteHeadNode(head):
+   if head == None or head.next == None:
+       return None
+  
+   head = head.next
+   head.prev = None
+   return head
+  
+n = int(input())
+l = list(map(int, input().split()))
+
+
+head = None
+for ele in l:
+   head = insertAtEnd(head, ele)
+  
+printLeftToRight(head)
+printRightToLeft(head)
+
+
+head = deleteHeadNode(head)
+
+
+printLeftToRight(head)
+printRightToLeft(head)
+
+#Sample Input 
+
+#8
+#9 8 7 6 5 4 3 2
+#Sample Output 
+
+#9 8 7 6 5 4 3 2
+#2 3 4 5 6 7 8 9 
+#8 7 6 5 4 3 2
+#2 3 4 5 6 7 8
